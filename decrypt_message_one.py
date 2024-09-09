8ae6a28144a37a2da1d8b32ccd0f6156e49bddcc
samra <samramujcinovic5@gmail.com>
2024-09-08 23:17:45 -0400

    Decrypted message one

--- decrypt_message_one.py
+++ decrypt_message_one.py
@@ -41,3 +41,27 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write code below
+
+
+# Create the reverse cipher
+reverse_cipher = {v: k for k, v in cipher.items()}
+
+# Function to decrypt the message
+def decrypt_message(encrypted_message, reverse_cipher):
+    decrypted_message = ""
+    for char in encrypted_message:
+        if char in reverse_cipher:
+            decrypted_message += reverse_cipher[char]
+        else:
+            decrypted_message += char  # Leave any characters that are not in the cipher unchanged
+    return decrypted_message
+
+# Open the encrypted file and read the message
+with open("encrypted_message_one.txt", 'r') as encrypted_file:
+    encrypted_message = encrypted_file.read()
+
+# Decrypt the message
+decrypted_message = decrypt_message(encrypted_message, reverse_cipher)
+
+# Print the decrypted message
+print(decrypted_message)
